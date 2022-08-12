4d37b03c414a70ff965009e7b5fb2fcf166e9695
ajaikamble10 <ajaysuhaskamble@gmail.com>
2022-08-12 12:25:08 +0530

    added get single note details

--- src/main/java/in/ajaykamble/springboot/controller/NotesController.java
+++ src/main/java/in/ajaykamble/springboot/controller/NotesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,10 @@ public class NotesController {
 	@PostMapping("/notes")
 	public ResponseEntity<Note> createNote(@RequestBody Note note){
 		return new ResponseEntity<Note>(nRepo.save(note),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/notes/{id}")
+	public ResponseEntity<Note> readNote(@PathVariable Long id){
+		return new ResponseEntity<Note>(nRepo.findById(id).get(), HttpStatus.OK);
 	}
 }
