073cab505dbc6d5e4b19430cd97647a4a8beac64
ajaikamble10 <ajaysuhaskamble@gmail.com>
2022-08-13 19:00:48 +0530

    final changes commit

--- src/main/java/in/ajaykamble/springboot/controller/NotesController.java
+++ src/main/java/in/ajaykamble/springboot/controller/NotesController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,16 @@ public class NotesController {
 	@GetMapping("/notes/{id}")
 	public ResponseEntity<Note> readNote(@PathVariable Long id){
 		return new ResponseEntity<Note>(nRepo.findById(id).get(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/notes/{id}")
+	public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id){
+		nRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping("/notes")
+	public ResponseEntity<Note> updateNote(@RequestBody Note note){
+		return new ResponseEntity<Note>(nRepo.save(note),HttpStatus.OK);
 	}
 }
